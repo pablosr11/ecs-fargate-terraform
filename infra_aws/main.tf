8770c5780728aefc673c08fd686c44be79cec6b9
ps <pablosr11@gmail.com>
2023-06-13 22:45:37 +0100

    Include vpc, first subnet, routing table and gateway to open it

--- infra_aws/main.tf
+++ infra_aws/main.tf
@@ -31,3 +31,58 @@ resource "aws_ecr_repository" "main" {
   }
 }
 
+## VPC, Subnets, Networking
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "clinikita-vpc"
+  }
+}
+
+# Subnets
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+# Open up the subnet to the internet
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "clinikita-igw"
+  }
+}
+
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name              = "clinikita-logs"
+  retention_in_days = 7
+}
--- infra_aws/variables.tf
+++ infra_aws/variables.tf
@@ -1,3 +1,14 @@
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
 
 variable "aws_region" {
   type        = string
